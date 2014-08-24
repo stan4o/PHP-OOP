ee4b94a8fa4af200859a25045110424e38caa8c3
Stanimir Panayotov <stanimirpanayotov2@gmail.com>
2014-08-24 18:14:20 +0300

    SeSeparated logic from html

--- index.php
+++ index.php
@@ -7,34 +7,13 @@ if (Session::exists('home')) {
 }
 
 $user = new User();
-$post = new Post();
 
-if ($post->find()) {
-    $data = $post->data();
-    echo $post->count();
+include 'includes/content/main-menu.php';
 
-    if ($post->count() > 1) {
-        foreach ($data as $post) {
-            ?>
-
-            <h3><?php echo $post->title; ?></h3>
-            <p>Date created: <em><time  datetime="<?php echo $post->created; ?>"><?php echo $post->created; ?></time></em></p>
-            <p><?php echo $post->body; ?></p>
-
-            <?php
-        }
-    } else {
-        $post = $post->data();
-        ?>
-
-        <h3><?php echo $post->title; ?></h3>
-        <p>Date created: <em><time  datetime="<?php echo $post->created; ?>"><?php echo $post->created; ?></time></em></p>
-        <p><?php echo $post->body; ?></p>
-
-        <?php
-    }
+if ($user->isLoggedIn()) {
+    include 'includes/user/menu.php';
 } else {
-    echo "There are no posts";
+    include 'includes/content/login-form.php';
 }
-?>
+
 
