7a240384a8b2ec39350125ef839e8ac036f428d7
Stanimir Panayotov <stanimirpanayotov2@gmail.com>
2014-08-24 18:16:22 +0300

    Added logic for updating posts

--- update.php
+++ update.php
@@ -6,47 +6,103 @@ $user = new User();
 
 if (!$user->isLoggedIn()) Redirect::to('index.php');
 
-if (Input::exists()) {
-    if (Token::check(Input::get('token'))) {
+if (Session::exists('update')) {
+    echo "<p>" . Session::flash('update') . "</p>";
+}
 
-        $validate = new Validate();
-        $validation = $validate->check($_POST, array(
-            'name' => array(
-                'required' => TRUE,
-                'min' => 2,
-                'max' => 50
-            )
-        ));
+$action = (Input::get('action')) ? Input::get('action') : NULL;
 
-        if ($validation->passed()) {
-            try {
-                $user->update(array(
-                    'name' => Input::get('name')
+if ($action === 'details') {
+    if (Input::exists()) {
+        if (Token::check(Input::get('token'))) {
+            $validate = new Validate();
+            $validation = $validate->check($_POST, array(
+                'name' => array(
+                    'required' => TRUE,
+                    'min' => 2,
+                    'max' => 50
+                )
+            ));
+
+            if ($validation->passed()) {
+                try {
+                    $user->update(array(
+                        'name' => Input::get('name')
+                    ));
+
+                    Session::flash('home', "Your details have been updated!");
+                    Redirect::to('index.php');
+
+                } catch (Exception $e) {
+                    die($e->getMessage());
+                }
+            } else {
+                Session::flash('update', implode("<br>", $validation->errors()));
+                Redirect::to('update.php');
+            }
+        }
+    }
+
+include 'includes/content/update-details-form.php';
+
+} else if ($action === 'posts' && $user->hasPermission('admin')) {
+    $post = new Post();
+
+    if (!$post->find()) {
+        Session::flash('create', 'No blog posts created. Create new one!');
+        Redirect::to('create.php');
+    }
+
+    $data = $post->data();
+
+    if (!$postId = Input::get('id')) {
+        include 'includes/content/update-posts.php';
+    } else {
+        if (!$post->find($postId)) Redirect::to('index.php');
+        $data = $post->data();
+
+        if (Input::exists()) {
+            if (Token::check(Input::get('token'))) {
+                $validate = new Validate();
+                $validation = $validate->check($_POST, array(
+                    'title' => array(
+                        'required' => TRUE,
+                        'min' => 2,
+                        'max' => 150
+                    ),
+                    'body' => array(
+                        'required' => TRUE,
+                        'min' => 2,
+                        'max' => 10000
+                    )
                 ));
 
-                Session::flash('home', "Your details have been updated!");
-                Redirect::to('index.php');
+                if ($validation->passed()) {
+                    try {
+                        $post->update(
+                            array(
+                                'title' => escape(Input::get('title')),
+                                'body' => escape(Input::get('body')),
+                                'created' => date("Y-m-d H:i:s")
+                            ),
+                            $data->id
+                        );
 
-            } catch (Exception $e) {
-                die($e->getMessage());
-            }
-        } else {
-            foreach ($validation->errors() as $error) {
-                echo $error . "</br>";
+                        Session::flash('update', 'The post has been updated!');
+                        Redirect::to('update.php?action=posts');
+
+                    } catch (Exception $e) {
+                        die($e->getMessage());
+                    }
+                } else {
+                    Session::flash('update', implode("<br>", $validation->errors()));
+                    Redirect::to("update.php?action=posts");
+                }
             }
         }
 
+        include 'includes/content/update-posts-form.php';
     }
+} else {
+    Redirect::to(404);
 }
-
-?>
-
-<form action="" method="post">
-    <div class="field">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" value="<?php echo $user->data()->name; ?>">
-    </div>
-
-    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
-    <input type="submit" value="Update">
-</form>
