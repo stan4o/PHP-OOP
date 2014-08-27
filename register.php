40e43123e6844e1c9552c1af9aed7efe51fb8d38
Stanimir Panayotov (stakata / stan4o) <stanimirpanayotov2@gmail.com>
2014-08-27 21:35:33 +0300

    Registering user now points to create.php with appropriate action

--- register.php
+++ register.php
@@ -2,83 +2,10 @@
 
 require_once 'core/init.php';
 
-if (Input::exists()) {
-    if (Token::check(Input::get('token'))) {
-        $validate = new Validate();
-        $validation = $validate->check($_POST, array(
-            'username' => array(
-                'required' => true,
-                'min' => 2,
-                'max' => 20,
-                'unique' => 'users'
-            ),
-            'password' => array(
-                'required' => true,
-                'min' => 6
-            ),
-            'password_again' => array(
-                'required' => true,
-                'matches' => 'password'
-            ),
-            'name' => array(
-                'required' => true,
-                'min' => 2,
-                'max' => 50
-            )
-        ));
-
-        if ($validation->passed()) {
-
-            $user = new User();
-            $salt = Hash::salt(32);
-
-            try {
-
-                $user->create(array(
-                    'username' => Input::get('username'),
-                    'password' => Hash::make(Input::get('password'), $salt),
-                    'salt' => $salt,
-                    'name' => Input::get('name'),
-                    'joined' => date("Y-m-d H:i:s"),
-                    'group' => 1
-                ));
-
-                Session::flash('home', 'You have been successfully registered!');
-                Redirect::to('index.php');
-
-            } catch (Exception $e) {
-                die($e->getMessage());
-            }
-        } else {
-            foreach ($validation->errors() as $error) {
-                echo $error . "</br>";
-            }
-        }
-    }
+if (Session::exists('register')) {
+    echo "<p>" . Session::flash('register') . "</p>";
 }
 
+include 'includes/content/create-users.php';
 ?>
 
-<form action="" method="post">
-    <div class="field">
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username" value="<?php echo escape(Input::get('username'));?>">
-    </div>
-
-    <div class="field">
-        <label for="password">Choose a password</label>
-        <input type="password" id="password" name="password">
-    </div>
-
-    <div class="field">
-        <label for="password_again">Repeat the password</label>
-        <input type="password" id="password_again" name="password_again">
-    </div>
-
-    <div class="field">
-        <label for="name">Enter a name</label>
-        <input type="text" id="name" name="name" value="<?php echo escape(Input::get('name'));?>">
-    </div>
-    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
-    <input type="submit" value="Register">
-</form>
