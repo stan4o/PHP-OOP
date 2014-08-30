6acd9ea59d1e595778c0edbeba8ba6e495c48392
Stanimir Panayotov (stakata / stan4o) <stanimirpanayotov2@gmail.com>
2014-08-30 13:07:54 +0300

    Refactoring file structure

--- includes/menus/user-menu.php
+++ includes/menus/user-menu.php
@@ -3,7 +3,7 @@
         <li><a href="profile.php?user=<?php echo escape($user->data()->username); ?>">Profile</a></li>
         <li><a href="update.php?action=details">Update Details</a></li>
         <li><a href="changepassword.php">Change Your Password</a></li>
-        <?php if ($user->hasPermission('admin')) include 'includes/admin/menu.php'; ?>
+        <?php if ($user->hasPermission('admin')) include 'includes/menus/admin-menu.php'; ?>
         <li><a href="logout.php">Logout</a></li>
     </ul>
 </div>
