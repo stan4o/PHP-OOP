580b60e1dcc0c31fdd4cdc8a4bae15b1319025dc
Stanimir Panayotov (stakata / stan4o) <stanimirpanayotov2@gmail.com>
2015-01-12 22:18:40 +0200

    Changed extension for logic separation

--- includes/menus/main-menu.tpl.php
+++ includes/menus/main-menu.tpl.php
@@ -1,9 +1,9 @@
-<nav>
+<div id="main-menu">
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="posts.php">Posts</a></li>
         <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact Me</a></li>
         <li><a href="portfolio.php">Portfolio</a></li>
-        <li><a href="contact.php">Contact</a></li>
     </ul>
-</nav>
+</div>
